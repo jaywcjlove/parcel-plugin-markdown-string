e7d8405691c75b36e8518b1fa8d2e5083b02ac5b
jaywcjlove <398188662@qq.com>
2018-04-29 21:13:35 +0800

    Modify test example.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const Bundler = require('parcel-bundler');
 const PluginReactMarkdown = require('../index.js');
 
-let bundler = new Bundler('./example/index.html', {
+let bundler = new Bundler('./test/example/index.html', {
     watch: true
 });
 
